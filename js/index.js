765a7d5a6a1a0167c02bf6976b8f5ea3fa0510b7
Kyle Qian <kylecqian@gmail.com>
2019-04-29 01:57:52 -0700

    First three pages

--- js/index.js
+++ js/index.js
@@ -1,3 +1,26 @@
 $(function() {
-  console.log('ready!');
+	var root = $('body');
+	var children = [];
+	var index = 0;
+
+	function advancePage() {
+		children[index].hide();
+		index++;
+		children[index].show();
+	}
+
+	// Store all reference to children and hide.
+	root.children().each(function(i) {
+		children.push($(this));
+		if (i != index) {
+			$(this).hide();
+		}
+	});
+
+	/*
+		Button registrations
+	*/
+	$('.button.simple-next').click(function(e) {
+		advancePage();
+	});
 });
